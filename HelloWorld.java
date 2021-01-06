63fb597fea81e47177994ef8d9579d2c65c28047
Antoine Quetin <47987950+appalouza@users.noreply.github.com>
2021-01-06 10:45:46 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-       System.out.println(" Welcome to the Jenkins App!"); 
+       System.out.println(" Welcome to the Jenkins App Antoine!"); 
     }
 }
